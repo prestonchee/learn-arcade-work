9d70de758ce84d0d666a8963216147eaf334c4f3
prestonchee <prestonchee1@gmail.com>
2023-01-11 10:58:22 -0700

    updated print functions output.

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,7 +1,7 @@
-print('Hello World')
-print("""This
-will print 
-multiple lines""")
+print('Hello World, my name is Preston')
+print("""Who 
+Knew that 
+program is fun""")
 print()
 print('Hello\rworld')
-print('\tThis adds a tab')
+print('\tThis function is for tabs \n but I am not sure if i will use it that often')
