f060d62aac03060e8b65db1c7573ca80ebb300e0
prestonchee <prestonchee1@gmail.com>
2023-03-16 18:23:22 -0600

    finish creating game

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -1,55 +1,62 @@
+"""
+First Drawing - Fishing Pond
+Preston Chee
+3/16/2023
+CS 1400
+"""
 import random
 import arcade
 import math
 
-# --- Constants ---
+# Constant Variables
 SPRITE_SCALING_PLAYER = 0.5
 SPRITE_SCALING_FISH = 0.5
 SPRITE_SCALING_SHOT = 1
 FISH_COUNT = 50
+SHOT_COUNT = 50
 
-SCREEN_WIDTH = 800
+# Set Screen size
+SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 600
 
-
+# Bad Sprite
 class Shot(arcade.Sprite):
 
     def __init__(self, filename, sprite_scaling):
-        """ Constructor. """
-        # Call the parent class (Sprite) constructor
+
+        # Constructor
         super().__init__(filename, sprite_scaling)
 
-        # Current angle in radians
+        # Set angle
         self.circle_angle = 0
 
-        # How far away from the center to orbit, in pixels
+        # Orbit radius
         self.circle_radius = 0
 
-        # How fast to orbit, in radians per frame
+        # Orbit Speed
         self.circle_speed = 0.008
 
-        # Set the center of the point we will orbit around
+        # Orbits center point
         self.circle_center_x = 0
         self.circle_center_y = 0
 
     def update(self):
-        """ Update the ball's position. """
-        # Calculate a new x, y
+        # x and y change
         self.center_x = self.circle_radius * math.sin(self.circle_angle) \
                         + self.circle_center_x
         self.center_y = self.circle_radius * math.cos(self.circle_angle) \
                         + self.circle_center_y
 
-        # Increase the angle in prep for the next round.
+        # Set angle to speed
         self.circle_angle += self.circle_speed
 
-
+# Good Sprite
 class Fish(arcade.Sprite):
 
     def __init__(self, filename, sprite_scaling):
 
         super().__init__(filename, sprite_scaling)
-
+        # Initiate change variables
         self.change_x = 0
         self.change_y = 0
 
@@ -59,7 +66,7 @@ class Fish(arcade.Sprite):
         self.center_x += self.change_x
         self.center_y += self.change_y
 
-        # If we are out-of-bounds, then 'bounce'
+        # If object hits the edge of screen move in different direction
         if self.left < 0:
             self.change_x *= -1
 
@@ -72,16 +79,15 @@ class Fish(arcade.Sprite):
         if self.top > SCREEN_HEIGHT:
             self.change_y *= -1
 
-
+# Main game class
 class MyGame(arcade.Window):
-    """ Our custom Window Class"""
 
     def __init__(self):
-        """ Initializer """
-        # Call the parent class initializer
+
+        # Parent class initializer
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
 
-        # Variables that will hold sprite lists
+        # Sprite List Variables
         self.player_list = None
         self.fish_list = None
         self.shot_list = None
@@ -90,16 +96,17 @@ class MyGame(arcade.Window):
         # Set up the player info
         self.player_sprite = None
         self.score = 0
-
-        # Don't show the mouse cursor
+        # Hide mouse cursor
         self.set_mouse_visible(False)
 
+        # Main background color set
         arcade.set_background_color(arcade.color.OCEAN_BOAT_BLUE)
 
-        self.error_sound = arcade.load_sound("pistol.wav")
+        # Create sound variable
+        self.gun_shot = arcade.load_sound("pistol.wav")
 
+    # Main game setup
     def setup(self):
-        """ Set up the game and initialize the variables. """
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
@@ -107,58 +114,52 @@ class MyGame(arcade.Window):
         self.shot_list = arcade.SpriteList()
         self.good_list = arcade.SpriteList()
 
-        # Score
-        self.score = 0
-
         # Set up the player
-        # Character image from kenney.nl
         self.player_sprite = arcade.Sprite("duck.png", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
 
-        # Create the coins
+        # Create the number of fishes to start off with
         for i in range(FISH_COUNT):
 
-            # Create the coin instance
-            # Coin image from kenney.nl
+            # Create the fish instance
             fish = Fish("fish.png", SPRITE_SCALING_FISH)
 
-            # Position the coin
+            # Set fish location
             fish.center_x = random.randrange(SCREEN_WIDTH)
             fish.center_y = random.randrange(SCREEN_HEIGHT)
             fish.change_x = random.randrange(-3, 4)
             fish.change_y = random.randrange(-3, 4)
 
-            # Add the coin to the lists
+            # Add the fish to list
             self.fish_list.append(fish)
 
-        for j in range(50):
-            # Create the coin instance
-            # Coin image from kenney.nl
+        for j in range(SHOT_COUNT):
+            # Create the fish instance
             shot = Shot("shot.png", SPRITE_SCALING_SHOT / 3)
 
-            # Position the center of the circle the coin will orbit
+            # Set center of the circle the shot will orbit
             shot.circle_center_x = random.randrange(SCREEN_WIDTH)
             shot.circle_center_y = random.randrange(SCREEN_HEIGHT)
 
-            # Random radius from 10 to 200
-            shot.circle_radius = random.randrange(10, 200)
+            # Set radius to range from 50 to 250
+            shot.circle_radius = random.randrange(50, 250)
 
-            # Random start angle from 0 to 2pi
+            # Start angle from 0 to 2pi
             shot.circle_angle = random.random() * 2 * math.pi
 
             self.shot_list.append(shot)
 
+    # Draw subclass that creates the images
     def on_draw(self):
-        """ Draw everything """
+
         arcade.start_render()
         self.fish_list.draw()
         self.player_list.draw()
         self.shot_list.draw()
 
-        # Put the text on the screen.
-
+        # Place text on screen for score and when the game end with final score
         if len(self.fish_list) > 0:
             output = f"Score: {self.score}"
             arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
@@ -168,25 +169,23 @@ class MyGame(arcade.Window):
             output = f"Score: {self.score}"
             arcade.draw_text(output, (SCREEN_WIDTH / 2) - 25, (SCREEN_HEIGHT / 2) - 50, arcade.color.WHITE, 15)
 
+    # Allow mouse movement
     def on_mouse_motion(self, x, y, dx, dy):
         """ Handle Mouse Motion """
 
-        # Move the center of the player sprite to match the mouse x, y
+        # Allow the person to move the good sprite until all good sprites are gone
         if len(self.fish_list) > 0:
             self.player_sprite.center_x = x
             self.player_sprite.center_y = y
 
     def update(self, delta_time):
-        """ Movement and game logic """
 
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
-
+        # Keep updating until all good sprites are gone
         if len(self.fish_list) > 0:
             self.fish_list.update()
             self.shot_list.update()
 
-        # Generate a list of all sprites that collided with the player.
+        # create list of both good and bad sprites that collide with the player sprite
             self.good_list = arcade.check_for_collision_with_list(self.player_sprite,
                                                                   self.fish_list)
             bad_list = arcade.check_for_collision_with_list(self.player_sprite,
@@ -200,14 +199,15 @@ class MyGame(arcade.Window):
                 self.score -= 1
                 shot.circle_center_x = random.randrange(SCREEN_HEIGHT)
                 shot.circle_center_y = random.randrange(SCREEN_WIDTH)
-                arcade.play_sound(self.error_sound)
+                arcade.play_sound(self.gun_shot)
 
-
+# Main method
 def main():
-    """ Main method """
+    # execute game
     window = MyGame()
     window.setup()
     arcade.run()
 
 
+# Execute main method
 main()
